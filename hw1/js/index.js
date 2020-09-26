14d4bedc4d5168d99f9c0f2c23802857cfdb2138
D S.chibrikov <d.s.chibrikov@nw.rt.ru>
2020-09-26 23:59:09 +0300

    hw1 done

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,2 +1,18 @@
 const g = document.querySelector('g');
 const circle = document.querySelector('circle');
+const sad = "M 150 200 Q 225 120 300 200";
+const happy = "M 150 200 Q 225 280 300 200";
+
+var checkbox = document.querySelector("input");
+var smile = document.querySelector("path");
+var placeholder = document.getElementById('placeholder');
+
+checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+        placeholder.innerHTML = "Happy";
+        smile.setAttribute("d", happy);
+    } else {
+        placeholder.innerHTML = "Sad";
+        smile.setAttribute("d", sad);
+    }
+});
